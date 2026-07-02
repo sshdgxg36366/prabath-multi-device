2fb74830f888f0f935d6bafdc270a450649d269a
sshdgxg36366 <oshadamd6@gmail.com>
2026-07-02 20:52:00 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const DATABASE = "mongodb+srv://oshadamd05_db_user:LzWjINfr32GpUo6v@cluster0.mu6qfvw.mongodb.net/?appName=Cluster0";
 const BOT_NUMBER = "94727634194"; // Rename whatsapp number rhm. (your whatsapp number)
-const SESSION_ID = "PRABATH-MD~6Q4ZIGH5#8NxOK7"; // ඔයාගෙ session id එක මෙතනින් දාන්න - https://prabathmd-official.vercel.app
+const SESSION_ID = "PRABATH-MD~zIy30mOwMTBLa8Q"; // ඔයාගෙ session id එක මෙතනින් දාන්න - https://prabathmd-official.vercel.app
 
 // ඉහත එවී "" මැදට එකතු කරන්න වෙනස් කිසිම දෙයක් වෙනස් නොවෙන්න ❌ . (Add "" in the middle of the above and ❌ don't change anything)
 
