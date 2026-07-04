e819f6ee127b0599b791693ec1277948808f5f2e
sshdgxg36366 <oshadamd6@gmail.com>
2026-07-04 13:00:36 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const DATABASE = "mongodb+srv://oshadamd05_db_user:LzWjINfr32GpUo6v@cluster0.mu6qfvw.mongodb.net/?appName=Cluster0"
+const DATABASE = "mongodb+srv://oshadamd6_db_user:PXJ4ZOdXfJQl3azz@cluster0.dsbxd8i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const BOT_NUMBER = "94727634194"
 const SESSION_ID = "PRABATH-MD~MlZw5OF1ifi34PC"
 
