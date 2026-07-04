212d4a8eb6b973ea3cd3864bce029d47a1af3fad
sshdgxg36366 <oshadamd6@gmail.com>
2026-07-04 12:45:46 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,10 +1,6 @@
-const DATABASE = "mongodb+const DATABASE = "" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
-const BOT_NUMBER = "" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
-const SESSION_ID = "PRABATH-MD~MlZw5OF1ifi34PC" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
-
-
-// ඉහත ඒව "" මැදට එකතු කරන්න වෙනත් කිසිම දෙයක් වෙනස් නොකරන්න ❌. (Add "" in the middle of the above and ❌ don't change anything else.)
-
+const DATABASE = "mongodb+srv://oshadamd05_db_user:LzWjINfr32GpUo6v@cluster0.mu6qfvw.mongodb.net/?appName=Cluster0"
+const BOT_NUMBER = "94727634194"
+const SESSION_ID = "PRABATH-MD~MlZw5OF1ifi34PC"
 
 const GITHUB_AUTH_TOKEN = DATABASE
 
